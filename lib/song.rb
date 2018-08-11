21070010f3cb26e6816993aefd51e45c83fd8be8
Kevin K <doilerjar@outlook.com>
2018-08-11 07:44:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
   end 
   
   def self.new_by_filename(filename)
-    song = Song.new(filename.split(" - ")[0])
-    song.artist = filename.split(" - ")[1]
+    song = Song.new(filename.split(" - ")[1])
+    song.artist = filename.split(" - ")[0]
     song
   end 
 
