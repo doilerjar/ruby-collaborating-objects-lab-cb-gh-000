741ce8b1a34f8bfb441b91e4dcf34fd479497d41
Kevin K <doilerjar@outlook.com>
2018-08-11 07:48:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,13 +4,9 @@ class Song
   
   def initialize(name)
     @name = name
-    @artist = nil
+    @artist = ""
   end 
-  
-  def artist_name=(name)
-    
-  end
-  
+
   def self.new_by_filename(filename)
     song = Song.new(filename.split(" - ")[1])
     song.artist.name = filename.split(" - ")[0]
